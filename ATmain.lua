3114e09cd60c3e18460b68d30c27a29cba0287ca
alfantasy <71088194+alfantasy@users.noreply.github.com>
2022-11-04 11:53:53 +0400

    Add files via upload

--- ATmain.lua
+++ ATmain.lua
@@ -3093,16 +3093,17 @@ function textSplit(str, delim, plain)
 end
 
 function sampev.onTextDrawSetString(id, text)
-	if id == 2061 and elm.checkbox.atrecon.v then
+	if id == 2060 and elm.checkbox.atrecon.v then
 		player_info = textSplit(text, "~n~")
 	end
 end
 
 function sampev.onShowTextDraw(id, data)
 	if (id >= 3 and id <= 38 or 
-	id == 266 or id == 2078 or 
+	id == 266 or id == 344 or
+	id == 2078 or 
 	id == 2050 or id == 351 or 
-	id == 104 or id == 105 or id == 106 or id == 107 or id == 108 or id == 110 or id == 111 or id == 109 or id == 130 or id == 139 or id == 138 or id == 122 or id == 132 or id == 350 or id == 133 or id == 103 or id == 134 or id == 135 or id == 136 or id == 137 or id == 126 or id == 114 or id == 113 or id == 119 or id == 131 or id == 132 or id == 129 or id == 123 or id == 117 or id == 112 or id == 116 or id == 119 or id == 120 or id == 118 or id == 121 or id == 124 or id == 125 or id == 127 or id == 128 or id == 115 or id == 2061 or id == 354 or id == 136 or id == 2056 or id == 140 or id == 141 or id == 142) and elm.checkbox.atrecon.v then
+	id == 104 or id == 105 or id == 106 or id == 107 or id == 108 or id == 110 or id == 111 or id == 109 or id == 130 or id == 139 or id == 138 or id == 122 or id == 132 or id == 350 or id == 133 or id == 103 or id == 134 or id == 135 or id == 136 or id == 137 or id == 126 or id == 114 or id == 113 or id == 119 or id == 131 or id == 132 or id == 129 or id == 123 or id == 117 or id == 112 or id == 116 or id == 119 or id == 120 or id == 118 or id == 121 or id == 124 or id == 125 or id == 127 or id == 128 or id == 115 or id == 2060 or id == 354 or id == 136 or id == 2056 or id == 140 or id == 141 or id == 142) and elm.checkbox.atrecon.v then
 		return false
 	end
 	if id == 2056 then  
